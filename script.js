b328fb7f588f1d55a11b6b5cb588078672c0e588
Ivan Bonnot <ivanbonnot@gmail.com>
2022-07-15 14:22:18 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let precioProducto = document.getElementById("precioProducto") 
 const calcular = document.getElementById('calcular')
 const mostrarCalculo = document.getElementById('calculo')
-let sinIva = 0
 
-calcular.addEventListener('click', validarInput(precioProducto.value))
+calcular.addEventListener('click', ()=>
+validarInput(precioProducto.value))
 
 function validarInput(precio) {
-    if( precio == "" && precio <= 0) {
-        console.log("ingrese un numero valido")
+    if( precio == "" && precio >= 0) {
+        console.log("ingrese un precio válido")
+    } else {
+        calculoSinIva(precioProducto.value)
+        calculoSeisCuotas(precioProducto.value)
+        calculoDoceCuotas(precioProducto.value)
     }
 }
 
-function calculoSinIva(){
-    console.log(precioProducto.value)
-    sinIva = precioProducto.value - (precioProducto.value * 0.21) 
+function calculoSinIva(precio){
+    console.log(precio)
+    const sinIva = precio - (precio * 0.21) 
     mostrarCalculo.innerHTML = sinIva
     console.log(sinIva)
     
@@ -22,5 +26,21 @@ function calculoSinIva(){
 //Precio sin iva
 
 //Precio en 6 cuotas sin int
+
+function calculoSeisCuotas(precio){
+    
+    const seisCuotas = (precio / 6 ) 
+    mostrarCalculo.innerHTML = seisCuotas
+    console.log(seisCuotas)
+    
+}
+
+function calculoDoceCuotas(precio){
+    
+    const doceCuotas = precio + (precio * 0.3) 
+    mostrarCalculo.innerHTML = doceCuotas
+    console.log(doceCuotas)
+    
+}
 
 //Precio en 12 cuotas con int de 30%
