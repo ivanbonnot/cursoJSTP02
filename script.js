5a928b142f0300791b8572d15eff876d73e767cd
Ivan Bonnot <ivanbonnot@gmail.com>
2022-07-19 18:52:20 -0300

    quinto commit

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-const precioProductoString = document.getElementById("precioProducto") 
+const precioProductoString = document.getElementById("precioProducto")
 const calcular = document.getElementById('calcular')
 const reiniciar = document.getElementById('reinicio')
 const mostrarCalculo = document.getElementById('calculo')
 
 reiniciar.disabled = true
 
-calcular.addEventListener('click', ()=> validarInput())
+calcular.addEventListener('click', () => validarInput())
+reiniciar.addEventListener('click', () => LimpiarHTML())
 
+//Validar input
 function validarInput() {
     const precioProducto = parseInt(precioProductoString.value)
-    console.log(precioProducto, typeof(precioProducto))
+    console.log(precioProducto, typeof (precioProducto))
 
-    if( precioProducto === '' || precioProducto <= 0 || isNaN(precioProducto)) {
+    if (precioProducto === '' || precioProducto <= 0 || isNaN(precioProducto)) {
         console.log("ingrese un precio válido")
     } else {
         calculo(precioProducto)
-        
     }
 }
 
 
 //Precio sin iva
-function calculo(precio){
-    const sinIva = precio - (precio * 0.21) 
-    const seisCuotas = Math.ceil((precio / 6 ) )
-    const doceCuotas = Math.ceil((precio + (precio * 0.3)) / 12 )
+function calculo(precio) {
+    const sinIva = precio - (precio * 0.21)
+    const seisCuotas = Math.ceil((precio / 6))
+    const doceCuotas = Math.ceil((precio + (precio * 0.3)) / 12)
     const totalEnDoce = doceCuotas * 12
 
-    
     agregarAlHTML(sinIva, seisCuotas, doceCuotas, totalEnDoce)
 }
 
@@ -38,4 +38,16 @@ function agregarAlHTML(sinIva, seisCuotas, doceCuotas, totalEnDoce) {
                          <p>El precio en 6 cuotas es: $${seisCuotas}. El total es el mismo </p>
                          <p>El precio en 12 cuotas con recargo es: $${doceCuotas}. El total es de $${totalEnDoce}</p>`
     mostrarCalculo.appendChild(parrafo);
+
+    reiniciar.disabled = false
+}
+
+
+//Limpiar formulario
+function LimpiarHTML() {
+    while (mostrarCalculo.firstChild) {
+        mostrarCalculo.removeChild(mostrarCalculo.firstChild);
+    }
+    reiniciar.disabled = true
+    precioProductoString = ''
 }
